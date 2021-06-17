2bf62bad8972d9659769e35c2d644281d142ec99
NCG2102010 <82468812+NCG2102010@users.noreply.github.com>
2021-06-17 10:05:48 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,94 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "252661a2-dd3f-432e-9216-5bc51875c2a9":
+            case "myImages.image0":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 1 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 3 1 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 1 1 1 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`;
+            case "efe6d1c7-02ce-4b61-4783-b80c7ff0536f":
+            case "myImages.image1":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 1 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 3 1 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 1 1 1 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`;
+            case "bf4af4da-8986-421f-ee7a-1815230f32cd":
+            case "myImages.image2":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 1 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 3 1 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 1 1 1 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`;
+            case "a9935f87-f6b7-4b47-fc55-8947a5cb6e95":
+            case "myImages.image3":return img`
+a a a a a a a a a a a a a a a a 
+a b b b b b b b b b b b b b b a 
+a b a a a a a a a a a a a a b a 
+a b a a b b a a a a b b a a b a 
+a b a a a a b a a b a a a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a b a a a a b a a a b a 
+a b a a a b a a a a b a a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a b b b b b b a a a b a 
+a b a a b a a a a a a b a a b a 
+a b a a a a a a a a a a a a b a 
+a b a a a a a a a a a a a a b a 
+a b b b b b b b b b b b b b b a 
+a a a a a a a a a a a a a a a a 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
